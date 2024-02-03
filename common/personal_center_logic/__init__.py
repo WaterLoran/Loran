0b7461bcfd350001b426f96be3ae517cddbac57a
WATER <1696746432@qq.com>
2024-02-03 13:41:37 +0800

    修改头像的请求体, 提取出入参

--- common/personal_center_logic/__init__.py
+++ common/personal_center_logic/__init__.py
@@ -4,15 +4,16 @@ import allure
 
 @Api.form_data
 @allure.step("修改用户头像")
-def mod_profile_picture():
+def mod_profile_picture(picture="猞猁.png"):
     req_url = "/dev-api/system/user/profile/avatar"
     req_method = "POST"
-    files = "猞猁.png"
+    files = picture
     data = {}
     rsp_check = {
         "msg": "操作成功",
         "code": 200
     }
+    auto_fill = False
     return locals()
 
 
